01071672e845e6c872d660673bc6625870b01b1d
Himanshu S <himanshu.sisodiya@ansys.com>
2022-10-30 12:19:54 +0530

    Android device changes

--- vaccine-portal/capacitor.config.ts
+++ vaccine-portal/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.himanshu',
-  appName: 'vaccine-portal',
+  appName: 'Vaccine Portal',
   webDir: 'www',
   bundledWebRuntime: false
 };
